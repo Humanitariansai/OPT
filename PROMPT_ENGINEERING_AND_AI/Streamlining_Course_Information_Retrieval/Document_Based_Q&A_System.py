c597f17c4dcf40b31483234a8f41cd79d3205381
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-04-26 15:36:49 -0400

    Update Document_Based_Q&A_System.py

--- PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
+++ PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
@@ -113,7 +113,7 @@ if uploaded_file is not None:
     
     index_name = "langchain-demo"
     global index
-    index = PineconeVectorStore.from_documents(split_docs, embeddings_model, index_name=index_name)
+    index = PineconeVectorStore.from_documents(documents, embeddings_model, index_name=index_name)
 
     # Define a function to find similar documents based on a given query
 

96c5c135cff57d6cd2712bbce721d72137c5f71a
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-05-03 15:27:16 -0400

    Update Document_Based_Q&A_System.py

--- PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
+++ PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
@@ -170,7 +170,25 @@ if st.button("Get Answer"):
     answer = get_answer(query)
     st.write(answer['result'])
 
+# Initialize chat history
+if "answers" not in st.session_state:
+    st.session_state.messages = []
 
+# Display chat messages from history on app rerun
+for answer in st.session_state.messages:
+    with st.chat_message("user"):
+        st.markdown(answer)
+    st.session_state.answers.append((content = answer))
+
+
+
+# # React to user input
+# if prompt := st.chat_input("What is up?"):
+#     # Display user message in chat message container
+#     with st.chat_message("user"):
+#         st.markdown(prompt)
+#     # Add user message to chat history
+#     st.session_state.messages.append({"role": "user", "content": prompt})
 
 
 

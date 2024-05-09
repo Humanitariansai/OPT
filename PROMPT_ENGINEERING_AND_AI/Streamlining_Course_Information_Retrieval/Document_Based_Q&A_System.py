be375924541f5192d875cbc62a8493b4a0d8e7de
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-05-09 11:59:18 -0400

    Update Document_Based_Q&A_System.py

--- PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
+++ PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
@@ -93,6 +93,16 @@ def vector_db():
     global index
 
     try:
+
+        loader = PyPDFLoader('uploaded_file')
+        docs = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, 
+                                                 chunk_overlap=50)
+        split_data = text_splitter.split_documents(docs)
+        indexes = PineconeVectorStore.from_documents(split_data, embeddings_model, index_name=index_name)
+    except Exception:
+        # If index retrieval fails or total vector count is 0, create vector
+        st.error(f"An error occurred: {e}")   
         # Try to retrieve vectors from existing index
         index = pc.Index(index_name)
         describe_stats = index.describe_index_stats()
@@ -102,16 +112,6 @@ def vector_db():
             raise Exception("Total Vector Count is 0")
         # If vectors exists, load it
         indexes = PineconeVectorStore.from_existing_index(index_name, embeddings_model)
-    except Exception:
-        # If index retrieval fails or total vector count is 0, create vector
-        st.error(f"An error occurred: {e}")
-        
-        loader = PyPDFLoader('uploaded_file')
-        docs = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, 
-                                                 chunk_overlap=50)
-        split_data = text_splitter.split_documents(docs)
-        indexes = PineconeVectorStore.from_documents(split_data, embeddings_model, index_name=index_name)
 
     return indexes
 

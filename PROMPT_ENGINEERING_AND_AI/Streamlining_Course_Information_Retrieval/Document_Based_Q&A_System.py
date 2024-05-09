7e052dab7721e5e9fc0791cd14cbfcee084ac996
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-05-09 11:41:33 -0400

    Update Document_Based_Q&A_System.py

--- PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
+++ PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
@@ -92,32 +92,26 @@ def vector_db():
     index_name = "langchain-demo"
     global index
 
-    # try:
-    #     # Try to retrieve vectors from existing index
-    #     index = pc.Index(index_name)
-    #     describe_stats = index.describe_index_stats()
-    #     total_vector_count = describe_stats['total_vector_count']
-    #     print(total_vector_count)
-    #     if total_vector_count == 0:
-    #         raise Exception("Total Vector Count is 0")
-    #     # If vectors exists, load it
-    #     indexes = PineconeVectorStore.from_existing_index(index_name, embeddings_model)
-    # except Exception:
-    #     # # If index retrieval fails or total vector count is 0, create vector
-    #     # st.error(f"An error occurred: {e}")
-    if uploaded_file is not None:
-#   # Process the uploaded file
+    try:
+        # Try to retrieve vectors from existing index
+        index = pc.Index(index_name)
+        describe_stats = index.describe_index_stats()
+        total_vector_count = describe_stats['total_vector_count']
+        print(total_vector_count)
+        if total_vector_count == 0:
+            raise Exception("Total Vector Count is 0")
+        # If vectors exists, load it
+        indexes = PineconeVectorStore.from_existing_index(index_name, embeddings_model)
+    except Exception:
+        # If index retrieval fails or total vector count is 0, create vector
+        st.error(f"An error occurred: {e}")
+        
         loader = PyPDFLoader('uploaded_file')
         docs = loader.load()
-    elif uploaded_file is None:
-        st.write("Please upload a file first")
-
-            
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, 
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, 
                                                  chunk_overlap=50)
-    split_data = text_splitter.split_documents(docs)
-    
-    indexes = PineconeVectorStore.from_documents(split_data, embeddings_model, index_name=index_name)
+        split_data = text_splitter.split_documents(docs)
+        indexes = PineconeVectorStore.from_documents(split_data, embeddings_model, index_name=index_name)
 
     return indexes
 

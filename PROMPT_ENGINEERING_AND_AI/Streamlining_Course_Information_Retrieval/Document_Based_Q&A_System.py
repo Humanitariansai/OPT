dbf3b3a06519c47d553671205a2ade9ab63d61fe
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-05-13 15:33:07 -0400

    Update Document_Based_Q&A_System.py

--- PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
+++ PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
@@ -25,6 +25,9 @@ from io import StringIO
 import PyPDF2  
 from langchain_community.document_loaders import PyPDFLoader,Docx2txtLoader
 import pdfplumber
+import os.path
+import pathlib
+
 
 # Set up the environment
 
@@ -83,22 +86,44 @@ os.environ["PINECONE_API_KEY"] = pinecone_api_key
 def vector_db():
     
     if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        data = uploaded_file.getvalue().decode('utf-8').splitlines()         
+        st.session_state["preview"] = ''
+        for i in range(0, min(5, len(data))):
+            st.session_state["preview"] += data[i]
+    preview = st.text_area("File Preview", "", height=150, key="preview")
+    upload_state = st.text_area("Upload State", "", key="upload_state")
+    def upload():
+        if uploaded_file is None:
+            st.session_state["upload_state"] = "Upload a file first!"
+        else:
+            data = uploaded_file.getvalue().decode('utf-8')
+            parent_path = pathlib.Path(__file__).parent.parent.resolve()           
+            save_path = os.path.join(parent_path, "data")
+            complete_name = os.path.join(save_path, uploaded_file.name)
+            destination_file = open(complete_name, "w")
+            destination_file.write(data)
+            destination_file.close()
+            st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
+    st.button("Upload file to Sandbox", on_click=upload)
+    
+    # if uploaded_file is not None:
         
-        # # # Check if uploaded_file is a list
-        # # if isinstance(uploaded_file, list):
-        # #     # If uploaded_file is a list, take the first element
-        # #     uploaded_file = uploaded_file[0]
+    #     # # # Check if uploaded_file is a list
+    #     # # if isinstance(uploaded_file, list):
+    #     # #     # If uploaded_file is a list, take the first element
+    #     # #     uploaded_file = uploaded_file[0]
             
-        # # Read the content of the uploaded file
-        # file_content = uploaded_file.read()
-        # # Create a file-like object from the content
-        # file_buffer = BytesIO(file_content)
-        # with pdfplumber.open(file_buffer) as file:
-        #     # all_pages = file.pages
-        #     # st.write(all_pages[0].extract_text())
-        #     docs = file.load_and_split()
-        file_path = os.path.join(os.getcwd(), uploaded_file.filename)
-        loader = PyPDFLoader(file_path)
+    #     # # Read the content of the uploaded file
+    #     # file_content = uploaded_file.read()
+    #     # # Create a file-like object from the content
+    #     # file_buffer = BytesIO(file_content)
+    #     # with pdfplumber.open(file_buffer) as file:
+    #     #     # all_pages = file.pages
+    #     #     # st.write(all_pages[0].extract_text())
+    #     #     docs = file.load_and_split()
+    #     file_path = os.path.join(os.getcwd(), uploaded_file.filename)
+        loader = PyPDFLoader('complete_name')
         docs = loader.load()
             
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, 

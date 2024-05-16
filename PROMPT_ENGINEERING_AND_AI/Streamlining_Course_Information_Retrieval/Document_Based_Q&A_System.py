d025ccc52d99b146f80ab96850bbf357d96c16ef
Ansh Aya <123016201+Anshhh369@users.noreply.github.com>
2024-05-16 14:44:59 -0400

    Update Document_Based_Q&A_System.py

--- PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
+++ PROMPT_ENGINEERING_AND_AI/Streamlining_Course_Information_Retrieval/Document_Based_Q&A_System.py
@@ -182,56 +182,56 @@ if query := st.chat_input("Ask your question here"):
 
 
 
-def upload_file_section():
-    st.title("🦜🔗Learning Assistance")
-    # File uploader for user to upload a document
-    uploaded_file = st.file_uploader("Upload your document", type=["pdf","docx"], accept_multiple_files = True)
-    return uploaded_file 
+# def upload_file_section():
+#     st.title("🦜🔗Learning Assistance")
+#     # File uploader for user to upload a document
+#     uploaded_file = st.file_uploader("Upload your document", type=["pdf","docx"], accept_multiple_files = True)
+#     return uploaded_file 
 
 
-def process_file_section(uploaded_file):
-    if uploaded_file is None:
-        st.warning("Please upload a file first.")
-        return
+# def process_file_section(uploaded_file):
+#     if uploaded_file is None:
+#         st.warning("Please upload a file first.")
+#         return
         
-    elif uploaded_file is not None:
-        if "vector_store" not in st.session_state:
-            # Initialize vector store
-            st.session_state.vector_store = vector_db(uploaded_file)
+#     elif uploaded_file is not None:
+#         if "vector_store" not in st.session_state:
+#             # Initialize vector store
+#             st.session_state.vector_store = vector_db(uploaded_file)
         
-def chat_section():
-    st.title("Chat with Me 🦜")
+# def chat_section():
+#     st.title("Chat with Me 🦜")
 
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+#     # Initialize chat history
+#     if "messages" not in st.session_state:
+#         st.session_state.messages = []
                 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+#     # Display chat messages from history on app rerun
+#     for message in st.session_state.messages:
+#         with st.chat_message(message["role"]):
+#             st.markdown(message["content"])
                         
-                # React to user input
-    if query := st.chat_input("Ask your question here"):
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(query)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": query})
+#                 # React to user input
+#     if query := st.chat_input("Ask your question here"):
+#         # Display user message in chat message container
+#         with st.chat_message("user"):
+#             st.markdown(query)
+#         # Add user message to chat history
+#         st.session_state.messages.append({"role": "user", "content": query})
                     
-        answer = get_answer(query)
-        result = answer['result']
+#         answer = get_answer(query)
+#         result = answer['result']
                         
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown(result)
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": result})
+#         # Display assistant response in chat message container
+#         with st.chat_message("assistant"):
+#             st.markdown(result)
+#             # Add assistant response to chat history
+#             st.session_state.messages.append({"role": "assistant", "content": result})
                 
-        def clear_messages():
-            st.session_state.messages = []
+#         def clear_messages():
+#             st.session_state.messages = []
         
-        st.button('Clear',on_click=clear_messages)
+#         st.button('Clear',on_click=clear_messages)
 
 
 
